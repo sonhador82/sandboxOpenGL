9d6d0b1ff147398bd103b491f83e5e082ae6cf79
Dmitriy Shelestovskiy <dmitriy.shelestovski@neurohive.io>
2020-03-31 23:31:48 +0300

    hocket app part one, drawing lines and points

--- app/src/main/java/ru/sonhador/initopengl/CustomRenderer.java
+++ app/src/main/java/ru/sonhador/initopengl/CustomRenderer.java
@@ -1,16 +1,99 @@
 package ru.sonhador.initopengl;
 
-import static android.opengl.GLES20.*;
-
+import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import ru.sonhador.initopengl.util.ShaderHelper;
+import ru.sonhador.initopengl.util.TextResourceReader;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
 class CustomRenderer implements Renderer {
+    private final Context context;
+    private final static int POSITIONAL_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private int program;
+
+    private static final String U_COLOR = "u_Color";
+    private int uColorLocation;
+
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+
+    CustomRenderer(Context context) {
+        this.context = context;
+        float[] tableVertices = {
+                -0.5f, -0.5f,
+                0.5f, 0.5f,
+                -0.5f, 0.5f,
+
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                0.5f, 0.5f,
+
+                // line 1
+                -0.5f, 0f,
+                0.5f, 0f,
+
+                // mallets
+                0f, -0.25f,
+                0f, 0.251f
+        };
+
+        vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        glClearColor(1.0f, 0.0f,0.0f,0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        String vertexShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        String fragmentShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        // validate program ONLY IN DEBUG
+        ShaderHelper.validateProgram(program);
+        glUseProgram(program);
+
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation,
+                POSITIONAL_COMPONENT_COUNT, GL_FLOAT,
+                false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
     }
 
     @Override
@@ -21,5 +104,16 @@ class CustomRenderer implements Renderer {
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniform4f(uColorLocation, 1f, 1f, 1f, 1f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glUniform4f(uColorLocation, 1f, 0f, 0f, 1f);
+        glDrawArrays(GL_LINES, 6, 2);
+
+        glUniform4f(uColorLocation, 0f, 0f, 1f, 1f);
+        glDrawArrays(GL_POINTS, 8, 2);
+
+        glUniform4f(uColorLocation, 1f, 0f, 0f, 1f);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
--- app/src/main/java/ru/sonhador/initopengl/MainActivity.java
+++ app/src/main/java/ru/sonhador/initopengl/MainActivity.java
@@ -1,7 +1,5 @@
 package ru.sonhador.initopengl;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
@@ -9,6 +7,8 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         if (supportES2) {
             // request OpenGl context
             glSurfaceView.setEGLContextClientVersion(2);
-            glSurfaceView.setRenderer(new CustomRenderer());
+            glSurfaceView.setRenderer(new CustomRenderer(this));
             renderSet = true;
         } else {
             Toast.makeText(this, "Device dont support OpenGL ES2", Toast.LENGTH_SHORT).show();
